43d934d84d9d34dd8afca35d838a8726673fae8c
hyungjung kim <gnl4277@gmail.com>
2016-03-30 15:31:33 +0900

    refactoring code

--- js/server_side_javascript/app_file.js
+++ js/server_side_javascript/app_file.js
@@ -14,33 +14,38 @@ app.set('view engine','jade');
 app.use(bodyParser.urlencoded({ extended: false }));
 //template
 app.get('/topic/new',function(req,res){
-  res.render('new');
+  fs.readdir('data',function(err,files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.render('new',{topics:files});
+  });
+
 });
 
 //route
-app.get('/topic',function(req,res){
+app.get(['/topic','/topic/:id'],function(req,res){
   fs.readdir('data',function(err,files){
     if(err){
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-      res.render('view',{topics:files});
-  })
-});
-app.get('/topic/:id',function(req,res){
-  var id = req.params.id;
-  fs.readdir('data',function(err,files){
-    if(err){
-      console.log(err);
-      res.status(500).send('Internal Server Error');
+    var id = req.params.id;
+    if(id){
+      //id값이 있을 때
+      fs.readFile('data/'+id,'utf8',function(err,data){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        }
+        res.render('view',{topics:files, title:id, description:data});
+      })
     }
-    fs.readFile('data/'+id,'utf8',function(err,data){
-      if(err){
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-      }
-      res.render('view',{topics:files, title:id, description:data});
-    })
+    else{
+      //id값이 없을 때
+        res.render('view',{topics:files, title:'welcome',description:'Hello Javascript for Server'});
+    }
   })
 });
 app.post('/topic',function(req,res){
@@ -52,7 +57,7 @@ app.post('/topic',function(req,res){
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-    res.send('Success');
+    res.redirect('/topic/'+title);
   });
 });
 
